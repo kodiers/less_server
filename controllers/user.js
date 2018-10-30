3222240dbbd594b9c18d49656b9c8c0c71d8f20c
Victor Yamchinov <kodiers@gmail.com>
2018-10-30 15:55:24 +0300

    Completed register

--- controllers/user.js
+++ controllers/user.js
@@ -15,7 +15,22 @@ exports.register = function (req, res) {
     if (!helpers.checkStringNotEmpty(passwordConfirmation)) {
         return res.status(400).send({error: {title: 'Password confirmation is empty!', detail: 'Password confirmation username'}});
     }
+    if (password !== passwordConfirmation) {
+        return res.status(400).send({error: {title: 'Password confirmation and password confirmation not same!', detail: 'Provide correct data'}});
+    }
     User.findOne({username: username}, function (error, user) {
-
+        if (error) {
+            return res.status(500).send({error: {title: 'Database error!', detail: 'Something wrong when query database!'}});
+        }
+        if (user) {
+            return res.status(400).send({error: {title: 'User already exists!!', detail: `Username with ${username} exists!`}});
+        }
+        const newUser = new User({username: username, password: password});
+        newUser.save(function (err) {
+            if (err) {
+                return res.status(500).send({error: {title: 'Database error!', detail: 'Something wrong when save user!'}});
+            }
+            return res.json({username: newUser.username, _id: newUser._id});
+        });
     });
 };
